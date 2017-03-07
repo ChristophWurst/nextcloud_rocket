54856f24242d850aca5a2112247e111797de4bb2
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-03-07 21:32:40 +0100

    Add karma&jasmine and some basic configuration
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ js/tests/karma.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration
+module.exports = function (config) {
+	config.set({
+		// frameworks to use
+		frameworks: [
+			'jasmine'
+		],
+
+		// list of files / patterns to load in the browser
+		files: [
+			'../../../../core/vendor/es6-promise/dist/es6-promise.js',
+			'../../../../core/vendor/jquery/dist/jquery.js',
+			'../../../../core/vendor/underscore/underscore.js',
+			'../../../../core/vendor/backbone/backbone.js',
+			'../../../../core/vendor/handlebars/handlebars.js',
+			{pattern: '../**/*.js', included: true},
+		],
+
+		// list of files to exclude
+		exclude: [
+			'../vendor/**/*.js',
+			'../init.js'
+		],
+
+		// preprocess matching files before serving them to the browser
+		preprocessors: {},
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: [
+			'progress'
+		],
+
+		// start these browsers
+		browsers: [
+			'PhantomJS'
+		]
+	});
+};
